0447544cd544734013572b0b771ff4651144871d
talahasi <rvshiriev@gmail.com>
2023-11-08 16:06:32 +0000

    Adds npm and webpack configs

---
+++ webpack.confg.js
@@ -0,0 +1,15 @@
+import path from 'path'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ]
+}
